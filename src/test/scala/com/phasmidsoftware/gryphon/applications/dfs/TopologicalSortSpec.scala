69d801106e90bb19523d277f9ca62136cdfe11b8
Robin Hillyard <r.hillyard@northeastern.edu>
2023-06-21 19:38:25 -0400

    Added cycle check for Topological Sort
    TopologicalSort: added acyclic method.

--- src/test/scala/com/phasmidsoftware/gryphon/applications/dfs/TopologicalSortSpec.scala
+++ src/test/scala/com/phasmidsoftware/gryphon/applications/dfs/TopologicalSortSpec.scala
@@ -15,7 +15,7 @@ class TopologicalSortSpec extends AnyFlatSpec with should.Matchers {
 
     behavior of "TopologicalSort"
 
-    it should "sort" in { // FIXME this isn't right.
+    it should "sort" in {
         val uy = resource("/dag.graph")
         val graphBuilder: GraphBuilder[Int, Unit, Unit] = new GraphBuilder[Int, Unit, Unit]()
         val z: Try[Iterable[DirectedEdge[Int, Unit]]] = graphBuilder.createEdgeListPair(uy)(DirectedEdge.apply[Int])
@@ -23,12 +23,24 @@ class TopologicalSortSpec extends AnyFlatSpec with should.Matchers {
         val gy: Try[Graph[Int, Unit, DirectedEdge[Int, Unit], Unit]] = graphBuilder.createGraphFromEdges[DirectedEdge[Int, Unit]](graph)(z)
         gy match {
             case Success(g) =>
-                val sorted: Seq[Int] = TopologicalSort.sort(g)
+                val sorted = TopologicalSort.sort(g)
                 sorted shouldBe List(3, 6, 0, 5, 2, 1, 4)
+                TopologicalSort.acyclic(graph.edges, sorted) shouldBe true
             case Failure(x) => throw x
         }
-
-
     }
 
+    it should "handle a directed graph" in {
+        val uy = resource("/directed.graph")
+        val graphBuilder = new GraphBuilder[Int, Unit, Unit]()
+        val z = graphBuilder.createEdgeListPair(uy)(DirectedEdge.apply[Int])
+        val graph = DirectedGraph[Int, Unit, DirectedEdge[Int, Unit], Unit]("DAG")
+        val gy = graphBuilder.createGraphFromEdges[DirectedEdge[Int, Unit]](graph)(z)
+        gy match {
+            case Success(g) =>
+                val sorted = TopologicalSort.sort(g)
+                TopologicalSort.acyclic(g.edges, sorted) shouldBe false
+            case Failure(x) => throw x
+        }
+    }
 }

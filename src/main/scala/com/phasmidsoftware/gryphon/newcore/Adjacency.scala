83e9b9427c9b6389d180ecd8c140d991ec2db456
Robin Hillyard <r.hillyard@northeastern.edu>
2023-07-14 12:45:23 -0400

    Further basic work
    Adjacency: added connect method.

--- src/main/scala/com/phasmidsoftware/gryphon/newcore/Adjacency.scala
+++ src/main/scala/com/phasmidsoftware/gryphon/newcore/Adjacency.scala
@@ -8,21 +8,47 @@ import scala.collection.immutable.{HashMap, TreeMap}
 
 /**
  * Trait to model the concept of adjacency.
+ * Adjacency is central to the representation of graphs and trees.
+ * By using adjacency lists (bags), we can traverse a graph in time O(|E|) where E is the number of edges.
+ * In terms of the number (n) of vertices, traversal takes O(nx) where x is the mean degree.
+ * By referring to the Erdös-Rényi model, we can show that x ~ log(n), thus graph traversal becomes O(n log(n)).
+ * See [[https://en.wikipedia.org/wiki/Erdős–Rényi_model]].
  *
  * @tparam V the attribute type of a vertex (node).
  * @tparam X the edge (connection) type.
  */
 trait Adjacency[V, X <: Connection[V]] {
   /**
-   * Method to yield all the connections from the vertex (node) identified by v.
+   * Method to yield all the connections (as a Bag) from the vertex (node) identified by v.
    *
    * @param v a V.
    * @return a Bag[X].
    */
   def adjacent(v: V): Bag[X]
 
+  /**
+   * Method to add a new key-value pair of V->Bag[X] to this Adjacency.
+   *
+   * @param vx a tuple of V -> Bag[X].
+   * @return a new Adjacency which includes the new key-value pair (vBx).
+   */
   def +(vx: (V, Bag[X])): Adjacency[V, X]
 
+  /**
+   * Method to add a new connection X at V.
+   *
+   * @param v a node (vertex) identifier [V].
+   * @param x a connection [X].
+   * @return a new Adjacency which includes the connection x at v.
+   */
+  def connect(v: V, x: X): Adjacency[V, X]
+
+  /**
+   * Method to construct a new Adjacency based on this.
+   *
+   * @param map the map to be used.
+   * @return an Adjacency[V, X].
+   */
   def unit(map: Map[V, Bag[X]]): Adjacency[V, X]
 }
 
@@ -35,12 +61,29 @@ abstract class AbstractAdjacency[V, X <: Connection[V]](map: Map[V, Bag[X]]) ext
    */
   def adjacent(v: V): Bag[X] = map(v)
 
-  def +(vx: (V, Bag[X])): Adjacency[V, X] = unit(map + vx)
+  /**
+   * Method to add a new key-value pair of V->Bag[X] to this Adjacency.
+   * Note that if the key value exists in this, its current bag will not be present in the result.
+   *
+   * @param vBx a tuple of V -> Bag[X].
+   * @return a new Adjacency which includes the new key-value pair (vBx).
+   */
+  def +(vBx: (V, Bag[X])): Adjacency[V, X] = unit(map + vBx)
+
+  /**
+   * Method to add a new key-value pair of V->Bag[X] to this Adjacency.
+   * Note that if the key value exists in this, its current bag will not be present in the result.
+   *
+   * @param v a node (vertex) identifier [V].
+   * @param x a connection X.
+   * @return a new Adjacency which includes the new key-value pair (vBx).
+   */
+  def connect(v: V, x: X): Adjacency[V, X] = this + (v -> (map.getOrElse(v, Bag.empty) + x))
 
 }
 
 case class OrderedAdjacency[V: Ordering, X <: Connection[V]](map: TreeMap[V, Bag[X]]) extends AbstractAdjacency[V, X](map) {
-  def unit(map: Map[V, Bag[X]]): Adjacency[V, X] = map match {
+  def unit(map: Map[V, Bag[X]]): OrderedAdjacency[V, X] = map match {
     case m: TreeMap[V, Bag[X]] => OrderedAdjacency(m)
     case _ => throw CoreException(s"OrderedAdjacency: unit: map must be a TreeMap")
   }
@@ -51,7 +94,7 @@ object OrderedAdjacency {
 }
 
 case class UnorderedAdjacency[V, X <: Connection[V]](map: HashMap[V, Bag[X]]) extends AbstractAdjacency[V, X](map) {
-  def unit(map: Map[V, Bag[X]]): Adjacency[V, X] = map match {
+  def unit(map: Map[V, Bag[X]]): UnorderedAdjacency[V, X] = map match {
     case m: HashMap[V, Bag[X]] => UnorderedAdjacency(m)
     case _ => throw CoreException(s"UnorderedAdjacency: unit: map must be a HashMap")
   }
--- src/test/scala/com/phasmidsoftware/gryphon/newcore/AdjacencySpec.scala
+++ src/test/scala/com/phasmidsoftware/gryphon/newcore/AdjacencySpec.scala
@@ -14,15 +14,29 @@ class AdjacencySpec extends AnyFlatSpec with should.Matchers {
   it should "ordered adjacent" in {
 
     val a: Adjacency[Int, Connection[Int]] = OrderedAdjacency.empty[Int, Connection[Int]] + (0 -> Bag.create(DirectedConnection(0, 1)))
-
     a.adjacent(0).iterator.next() shouldBe DirectedConnection(0, 1)
   }
 
   it should "unordered adjacent" in {
 
     val a: Adjacency[Int, Connection[Int]] = UnorderedAdjacency.empty[Int, Connection[Int]] + (0 -> Bag.create(DirectedConnection(0, 1)))
-
     a.adjacent(0).iterator.next() shouldBe DirectedConnection(0, 1)
+  }
+
+  it should "+" in {
+    val a: Adjacency[Int, Connection[Int]] = UnorderedAdjacency.empty[Int, Connection[Int]] + (0 -> Bag.create(DirectedConnection(0, 1)))
+    a.adjacent(0).iterator.next() shouldBe DirectedConnection(0, 1)
+    val b = a + (0 -> Bag.create(DirectedConnection(0, 2)))
+    b.adjacent(0).iterator.toList shouldBe List(DirectedConnection(0, 2))
+  }
+
+  it should "connect" in {
+    val a: Adjacency[Int, Connection[Int]] = UnorderedAdjacency.empty[Int, Connection[Int]] + (0 -> Bag.create(DirectedConnection(0, 1)))
+    a.adjacent(0).iterator.next() shouldBe DirectedConnection(0, 1)
+    val b = a connect(0, DirectedConnection(0, 2))
+    val connexions = b.adjacent(0).iterator.toList
+    connexions.contains(DirectedConnection(0, 2)) shouldBe true
+    connexions.contains(DirectedConnection(0, 1)) shouldBe true
   }
 
 }

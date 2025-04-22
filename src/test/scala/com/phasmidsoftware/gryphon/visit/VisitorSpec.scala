e6391339d191164a62b96369903799f46fdbad00
Robin Hillyard <r.hillyard@northeastern.edu>
2025-04-22 12:03:50 -0400

    More updates to VisitorSpec

--- src/test/scala/com/phasmidsoftware/gryphon/visit/VisitorSpec.scala
+++ src/test/scala/com/phasmidsoftware/gryphon/visit/VisitorSpec.scala
@@ -16,71 +16,92 @@ class VisitorSpec extends AnyFlatSpec with should.Matchers {
   behavior of "PostVisitor"
 
   it should "visitPost and ignore visitPre" in {
-    val target: PostVisitor[Int, Queue[Int]] = PostVisitor()
-    val t1 = target.visitPost(1)
-    t1 shouldBe PostVisitor(Queue(1))
-    val t2 = t1.visitPre(1)
-    t2 shouldBe t1
-    val t3 = t2.visitPost(2)
-    t3 shouldBe PostVisitor(Queue(1, 2))
+    Using(PostVisitor[Int, Queue[Int]]()) {
+      target =>
+        val t1 = target.visitPost(1)
+        t1.journal shouldBe Seq(1)
+        val t2 = t1.visitPre(1)
+        t2 shouldBe t1
+        val t3 = t2.visitPost(2)
+        t3.journal shouldBe Seq(1, 2)
+    }
   }
 
   it should "visitPre twice to StringBuilder" in {
-    val target: PostVisitor[Int, StringBuilder] = PostVisitor()
-    val t2 = target.visitPost(1)
-    t2.journal.toString() shouldBe "1\n"
-    val t3 = t2.visitPost(2)
-    t3.journal.toString() shouldBe "1\n2\n"
+    Using(PostVisitor[Int, StringBuilder]()) {
+      target =>
+        val t1 = target.visitPost(1)
+        t1.journal.toString shouldBe "1\n"
+        val t2 = t1.visitPre(1)
+    }
   }
 
   it should "implement visitPost twice and journal" in {
-    val target = Visitor.createPost[Int]
-    target.visitPre(1) shouldBe target
-    val t1 = target.visitPost(1)
-    t1.journal shouldBe Seq(1)
-    val t2 = t1.visitPost(2)
-    t2.journal shouldBe Queue(1, 2)
+    Using(Visitor.createPost[Int]) {
+      target =>
+        val t1 = target.visitPost(1)
+        t1.journal shouldBe Seq(1)
+        val t2 = t1.visitPost(2)
+        t2.journal shouldBe Queue(1, 2)
+    }
   }
 
   it should "implement reversePost" in {
-    val target = Visitor.reversePost[Int]
-    target.visitPre(1) shouldBe target
-    val t1 = target.visitPost(1)
-    t1 shouldBe PostVisitor(List(1))
-    val t2 = t1.visitPost(2)
-    t2 shouldBe PostVisitor(List(2, 1))
+    Using(Visitor.reversePost[Int]) {
+      target =>
+        val t1 = target.visitPost(1)
+        t1.journal shouldBe List(1)
+        val t2 = t1.visitPost(2)
+        t2.journal shouldBe List(2, 1)
+    }
   }
 
   behavior of "PreVisitor"
 
   it should "visitPre twice" in {
-    val target: PreVisitor[Int, Queue[Int]] = PreVisitor()
-    val t2 = target.visitPre(1)
-    t2 shouldBe PreVisitor(Queue(1))
-    val t3 = t2.visitPre(2)
-    t3 shouldBe PreVisitor(Queue(1, 2))
+    Using(PreVisitor[Int, Queue[Int]]()) {
+      target =>
+        val t2 = target.visitPre(1)
+        t2 shouldBe PreVisitor(Queue(1))
+        t2.journal shouldBe Seq(1)
+        val t3 = t2.visitPre(2)
+        t3 shouldBe PreVisitor(Queue(1, 2))
+        t3.journal shouldBe Seq(1, 2)
+    }
   }
 
   it should "visitPre twice to StringBuilder" in {
-    val target: PreVisitor[Int, StringBuilder] = PreVisitor()
-    val t2 = target.visitPre(1)
-    t2.journal.toString() shouldBe "1\n"
-    val t3 = t2.visitPre(2)
-    t3.journal.toString() shouldBe "1\n2\n"
+    Using(PreVisitor[Int, StringBuilder]()) {
+      target =>
+        val t1 = target.visitPre(1)
+        t1.journal.toString shouldBe "1\n"
+        val t2 = t1.visitPre(2)
+        t2.journal.toString shouldBe "1\n2\n"
+    }
   }
 
   it should "visitPre twice to FileWriter" in {
-    val target: PreVisitor[Int, FileWriter] = PreVisitor()
-    val t2 = target.visitPre(1)
-    t2.visitPre(2).close()
+    Using(PreVisitor[Int, FileWriter]()) {
+      target =>
+        val t1 = target.visitPre(1)
+        t1.journal.toString shouldBe "1\n"
+        val t2 = t1.visitPre(2)
+        t2.journal.toString shouldBe "1\n2\n"
+    }
   }
 
   it should "visitPre twice to named FileWriter" in {
     val journal = new FileWriter("test.txt")
-    val target: PreVisitor[Int, FileWriter] = PreVisitor(journal)
-    val t2 = target.visitPre(1)
-    val t3 = t2.visitPre(2)
-    t3.journal.close()
+    Using(PreVisitor[Int, FileWriter](journal)) {
+      target =>
+        val t1 = target.visitPre(1)
+        t1.journal.toString shouldBe "1\n"
+        val t2 = t1.visitPre(2)
+        t2.journal.toString shouldBe "1\n2\n"
+      // NOTE: no need to close the journal because it's closed by the visitor in a Using block.
+      // journal.close()
+    }
+    a[java.io.IOException] should be thrownBy journal.write("done")
   }
 
   it should "implement visitPre twice and journal" in {
@@ -171,10 +192,12 @@ class VisitorSpec extends AnyFlatSpec with should.Matchers {
   behavior of "Visitor"
 
   it should "preFunc" in {
-    val target: PreVisitor[Int, Queue[Int]] = PreVisitor()
-    val queue = Queue.empty[Int]
-    val a1: Option[Queue[Int]] = target.preFunc(1)(queue)
-    a1 shouldBe Some(Queue(1))
+    Using(PreVisitor[Int, Queue[Int]]()) {
+      target =>
+        val queue = Queue.empty[Int]
+        val a1: Option[Queue[Int]] = target.preFunc(1)(queue)
+        a1 shouldBe Some(Queue(1))
+    }
   }
 
   it should "join 1" in {
@@ -197,15 +220,19 @@ class VisitorSpec extends AnyFlatSpec with should.Matchers {
   }
 
   it should "join 3" in {
-    val target: Visitor[Int, List[Int]] = Visitor.preAndPost[Int]
-    val z1: Visitor[Int, List[Int]] = target.visitPre(1)
-    z1.visitPost(2).journal shouldBe List(2, 1)
+    Using(Visitor.preAndPost[Int]) {
+      preVisitor =>
+        val z1: Visitor[Int, List[Int]] = preVisitor.visitPre(1)
+        z1.visitPost(2).journal shouldBe List(2, 1)
+    }
   }
 
   it should "postFunc" in {
-    val target: PostVisitor[Int, Queue[Int]] = PostVisitor()
-    val queue = Queue.empty[Int]
-    target.postFunc(1)(queue) shouldBe Some(Queue(1))
+    Using(PostVisitor[Int, Queue[Int]]()) {
+      target =>
+        val queue = Queue.empty[Int]
+        val a1: Option[Queue[Int]] = target.postFunc(1)(queue)
+        a1 shouldBe Some(Queue(1))
+    }
   }
-
 }
